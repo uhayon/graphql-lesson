3e50d64aa4995a38e54fdb243b6438e47779acb5
Uriel Hayon <uri.a77aque@gmail.com>
2019-09-22 13:22:46 -0300

    Cart Item count & Adding items to cart with Apollo

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient, gql } from 'apollo-boost';
+import { ApolloClient } from 'apollo-boost';
 
 import { store, persistor } from './redux/store';
+import { resolvers, typeDefs } from './graphql/resolvers';
 
 import './index.css';
 import App from './App';
@@ -21,7 +22,17 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: httpLink,
-  cache
+  cache,
+  typeDefs,
+  resolvers
+});
+
+client.writeData({
+  data: {
+    cartHidden: true,
+    cartItems: [],
+    itemCount: 0
+  }
 });
 
 ReactDOM.render(
